e919f4e352f6039a49f0dd6bc26bea9a18d99709
Michael Beukman <michael.beukman@gmail.com>
2020-03-13 14:20:45 +0200

    added no daemon

---
+++ app/src/main/java/com/sd/a3kleingroup/MySentFiles.java
@@ -0,0 +1,13 @@
+package com.sd.a3kleingroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MySentFiles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_sent_files);
+    }
+}
