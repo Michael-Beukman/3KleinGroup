645c5b168e351d792094c1a01ed9866f8d7ff868
Matkicail <mk6420@gmail.com>
2020-04-28 12:14:12 +0200

    empty class at current

---
+++ app/src/main/java/com/sd/a3kleingroup/classes/Sending/PublicFileSend.java
@@ -0,0 +1,6 @@
+package com.sd.a3kleingroup.classes.Sending;
+
+//This class will be extremely similar to SendLocalFile.
+//It is mainly in case they differ at a later date
+public class PublicFileSend {
+}
