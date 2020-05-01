547a0f726e20744e39f7b226db7d6affd0251cf2
Matkicail <mk6420@gmail.com>
2020-05-01 10:40:03 +0200

    added a todo

--- app/src/main/java/com/sd/a3kleingroup/PublicFileSendActivity.java
+++ app/src/main/java/com/sd/a3kleingroup/PublicFileSendActivity.java
@@ -40,7 +40,7 @@ public class PublicFileSendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_public_file_send);
-
+        // TODO: 2020/05/01 add interface for user 
 
 
     }
