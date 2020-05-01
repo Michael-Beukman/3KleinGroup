99bb2efcd7bf72025e82ac4693bd59b19e27811e
Matkicail <mk6420@gmail.com>
2020-05-01 11:03:53 +0200

    getDownloadURL added - a bit unsure here

--- app/src/main/java/com/sd/a3kleingroup/PublicFileSendActivity.java
+++ app/src/main/java/com/sd/a3kleingroup/PublicFileSendActivity.java
@@ -9,15 +9,21 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.sd.a3kleingroup.classes.db.dbPublicFiles;
+
+import java.io.File;
 
 /**
     This uses requestCode 10 - if someone else is currently using requestCode 10, I will change mine.
@@ -35,6 +41,7 @@ public class PublicFileSendActivity extends AppCompatActivity {
     private dbPublicFiles thisFile; //so that have access to the constructor and the hash map for adding it to the firestore db
     String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
     String TAG = "Public File Send Activity";
+    Uri downloadURL = null; // reset this to null after each upload and it's info no longer needed. That way we can tell if we got the URL
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,8 +90,8 @@ public class PublicFileSendActivity extends AppCompatActivity {
     /*
     Actually sending it to storage now - uploading
      */
-    public void uploadFile(Uri fileChosen){
-        StorageReference chosenReference = storageReference.child(userID); //send this to this user's folder.
+    public void uploadFile(Uri fileChosen){ // assume that this is created using the Uri.fromFile(new File(path)) method
+        StorageReference chosenReference = storageReference.child(userID + fileChosen.getLastPathSegment()); //send this to this user's folder.
         UploadTask uploadTask = chosenReference.putFile(fileChosen);
         uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
@@ -97,6 +104,38 @@ public class PublicFileSendActivity extends AppCompatActivity {
             public void onFailure(@NonNull Exception e) {
                 Log.d(TAG, "Failed to upload the file");
                 Toast.makeText(PublicFileSendActivity.this, "File failed to upload", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = ( 100 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                //Show this somewhere
+                Log.d(TAG, progress + "%" );
+            }
+        });
+    }
+    public void getDownloadURL(Uri fileUploaded){
+        final StorageReference reference = storageReference.child(userID + fileUploaded); // TODO: 2020/05/01 check that this is fine
+        UploadTask uploadTask = reference.putFile(fileUploaded);
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if(!task.isSuccessful()){
+                    throw task.getException();
+
+                }
+                return reference.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if(task.isSuccessful()){
+                    downloadURL = task.getResult();
+                    Log.d(TAG, "Success in getting a download Uri");
+                }
+                else{
+                    Log.d(TAG, "Failed to get the download Uri");
+                }
             }
         });
     }
