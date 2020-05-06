2cebcdb3abceeff89257f09b1bcbebd909b4bde4
Michael Beukman <michael.beukman@gmail.com>
2020-05-06 10:25:15 +0200

    Did some funny stuff with report.xml in a python script

--- lol.py
+++ lol.py
@@ -3,5 +3,5 @@ with open('app/build/reports/coverage/debug/report.xml', 'r') as f:
     s = f.read()
     x = re.sub('<package name="com/sd/a3kleingroup">[\s\S\n]*</package>', 'ree', s)
     print(x)
-    with open('/app/build/reports/coverage/debug/report.xml', 'w') as f:
+    with open('app/build/reports/coverage/debug/report.xml', 'w') as f:
         f.write(x)
