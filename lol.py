d0f6774e272effe2c8e9b89b3ae912f6b23f9ee4
Michael Beukman <michael.beukman@gmail.com>
2020-05-06 11:38:54 +0200

    The previous one didn't really work

--- lol.py
+++ lol.py
@@ -1,11 +1,13 @@
 import re
-with open('app/build/reports/coverage/debug/report.xml', 'r') as f:
+with open('app/build/reports/coverage/tmp/report.xml', 'r') as f:
     s = f.read()
     x = re.sub('<package name="com/sd/a3kleingroup">[\s\S\n]*</package>', '', s)
     names_to_ignore = ['Filter', 'Holder', 'RecyclerHolder', 'MyFile']
-    for n in names_to_ignore:
-        x = re.sub('<class name=".*'+str(n)+'">[\s\S\n]*</class>', '', x)
-        x = re.sub('<sourcefile name="'+str(n)+'.java">[\s\S\n]*</sourcefile>', '', x)
+    # for n in names_to_ignore:
+        # x = re.sub('<class name=".*'+str(n)+'">[\s\S\n]*</class>', '', x)
+        # x = re.sub('<sourcefile name="'+str(n)+'.java">[\s\S\n]*</sourcefile>', '', x)
+        # break
+
     print(x)
     with open('app/build/reports/coverage/debug/report.xml', 'w') as f:
         f.write(x)
