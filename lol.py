cc77a8f6687b75141d4781de835b53586e4976e3
Michael Beukman <michael.beukman@gmail.com>
2020-05-06 10:53:59 +0200

    Did some funny stuff with report.xml in a python script

--- lol.py
+++ lol.py
@@ -1,7 +1,11 @@
 import re
 with open('app/build/reports/coverage/debug/report.xml', 'r') as f:
     s = f.read()
-    x = re.sub('<package name="com/sd/a3kleingroup">[\s\S\n]*</package>', 'ree', s)
+    x = re.sub('<package name="com/sd/a3kleingroup">[\s\S\n]*</package>', '', s)
+    names_to_ignore = ['Filter', 'Holder', 'RecyclerHolder', 'MyFile']
+    for n in names_to_ignore:
+        x = re.sub('<class name=".*'+str(n)+'">[\s\S\n]*</class>', '', x)
+        x = re.sub('<sourcefile name="'+str(n)+'.java">[\s\S\n]*</sourcefile>', '', x)
     print(x)
     with open('app/build/reports/coverage/debug/report.xml', 'w') as f:
         f.write(x)
