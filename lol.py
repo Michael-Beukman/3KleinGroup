0db91a4d9229a3d3a14d38bf699a3dc0b019d93f
Michael Beukman <michael.beukman@gmail.com>
2020-05-13 14:20:17 +0200

    different coverage hacker

--- lol.py
+++ lol.py
@@ -1,3 +1,4 @@
+"""
 import re
 with open('app/build/reports/coverage/debug/report.xml', 'r') as f:
     s = f.read()
@@ -11,3 +12,32 @@ with open('app/build/reports/coverage/debug/report.xml', 'r') as f:
     print(x)
     with open('app/build/reports/coverage/debug/report.xml', 'w') as f:
         f.write(x)
+"""
+from xml.etree.ElementTree import ElementTree
+import re
+# New
+def change(name='app/build/reports/coverage/debug/report.xml'):
+    tree = ElementTree()
+    tree.parse(name)
+    regexes=[".*/classes/db/.*", '.*/.*Activity.*']
+
+    
+    for p in tree.findall('package'):
+             name = p.attrib['name']; print (name)
+             for c in p.findall('class'):
+                #   print(c.attrib)
+                  #p.remove(c)
+                  for r in regexes:
+                      if re.fullmatch(r, c.attrib['name']): 
+                          p.remove(c); 
+                          print("removing ", c.attrib['name'], re.match(r, c.attrib['name']))
+             for s in p.findall('sourcefile'):
+                # print(s.attrib['name'])
+                for r in regexes:
+                    if re.fullmatch(r, p.attrib['name'] + '/' + s.attrib['name']): 
+                        p.remove(s); 
+                        print("removing ", p.attrib['name'] + '/' + s.attrib['name'], r, re.fullmatch(r, p.attrib['name'] + '/' + s.attrib['name']))
+    # write again
+    tree.write(name)
+
+change() #
