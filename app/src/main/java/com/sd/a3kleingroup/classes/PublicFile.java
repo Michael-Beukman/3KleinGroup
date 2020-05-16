f0e21bf9c749dd9a69c0533b937e72a649ee2395
Matkicail <mk6420@gmail.com>
2020-05-16 11:06:38 +0200

    changed how public files are structured to allow for viewing of who has looked at your files.

--- app/src/main/java/com/sd/a3kleingroup/classes/PublicFile.java
+++ app/src/main/java/com/sd/a3kleingroup/classes/PublicFile.java
@@ -3,6 +3,7 @@ package com.sd.a3kleingroup.classes;
 import android.net.Uri;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import kotlin.NotImplementedError;
 
@@ -15,18 +16,19 @@ public class PublicFile {
     private String filepath;
     private Uri fileuri;
     private String userid;
-
+    private ArrayList<String> viewers = new ArrayList<>();
 
     // actual File contents and everything
     private File file;
 
     //done in the same order as it is in firebase currently
-    public PublicFile(String encryptionkey, String filename, String filepath, Uri fileuri, String userid) {
+    public PublicFile(String encryptionkey, String filename, String filepath, Uri fileuri, String userid, ArrayList<String> viewers) {
         this.encryptionkey = encryptionkey;
         this.filename = filename;
         this.filepath = filepath;
         this.fileuri = fileuri;
         this.userid = userid;
+        this.viewers = viewers;
     }
 
     /**
@@ -73,5 +75,9 @@ public class PublicFile {
     public void setUserid(String userid) {
         this.userid = userid;
     }
+
+    public ArrayList<String> getViewers() { return viewers; }
+
+    public void setViewers(ArrayList<String> viewers) { this.viewers = viewers; } //get this from the database when managing files, it is one of the only differences between this and dbPublicFiles in what they contain.
 }
 
