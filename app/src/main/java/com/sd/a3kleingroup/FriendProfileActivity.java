3f3039f40b700e2e4c7672c004aa6f05356ecd89
Guy Axelrod <g.axelrod1@gmail.com>
2020-05-17 23:27:24 +0200

    updated some friends UI

--- app/src/main/java/com/sd/a3kleingroup/FriendProfileActivity.java
+++ app/src/main/java/com/sd/a3kleingroup/FriendProfileActivity.java
@@ -1,34 +1,50 @@
 package com.sd.a3kleingroup;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NavUtils;
 import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.sd.a3kleingroup.classes.BaseActivity;
+import com.sd.a3kleingroup.classes.Callback;
+import com.sd.a3kleingroup.classes.MyError;
 import com.sd.a3kleingroup.classes.UnfriendDialogFragment;
+import com.sd.a3kleingroup.classes.db.dbPublicFiles;
 import com.sd.a3kleingroup.classes.db.dbUser;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class FriendProfileActivity extends BaseActivity implements UnfriendDialogFragment.UnfriendDialogListener {
 
     private dbUser friend;
     private String myID;
     private TextView txtName, txtEmail, txtNumPublic;
+    private RecyclerView recyclerView;
+    private FriendProfileActivity.RecyclerAdapter mAdapter;
     private FloatingActionButton fab;
     private FirebaseFirestore db;
+    private ArrayList<dbPublicFiles> publicFiles;
     private String TAG = "FriendProfile_TAG";
 
     @Override
@@ -45,10 +61,12 @@ public class FriendProfileActivity extends BaseActivity implements UnfriendDialo
         getSupportActionBar().setTitle("Profile");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        db = FirebaseFirestore.getInstance();
+
         setElements();
         setFab();
-
-        db = FirebaseFirestore.getInstance();
+        setupRV();
+        getFileDataFromFirestore();
     }
 
     private void setElements() {
@@ -58,7 +76,7 @@ public class FriendProfileActivity extends BaseActivity implements UnfriendDialo
         txtEmail.setText(friend.getName());
         txtName.setText(friend.getEmail());
 
-        //We need to query Firestore before this can be set
+        //We need to query Firestore before this text can be set
         txtNumPublic = findViewById(R.id.txtNumPublicFiles);
     }
 
@@ -130,5 +148,119 @@ public class FriendProfileActivity extends BaseActivity implements UnfriendDialo
                 });
     }
 
-    //TODO: recycler view with public files
+    private void setupRV() {
+        recyclerView = findViewById(R.id.recycler_view);
+        publicFiles = new ArrayList<>();
+        mAdapter = new FriendProfileActivity.RecyclerAdapter(this, publicFiles, new FriendProfileActivity.AdapterListener() {
+            //Callback for when specific public file is selected
+            @Override
+            public void onFileSelected(dbPublicFiles file) {
+                Log.d(TAG, "selected " + file.getFileName());
+                //TODO: add user to viewed of doc file.getID()?
+                downloadFile(file);
+            }
+        });
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        //recyclerView.addItemDecoration(new MyDividerItemDecoration(this, DividerItemDecoration.VERTICAL, 36));
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    private void getFileDataFromFirestore() {
+        //Callback for when data for a single public file is retrieved
+        Callback cbPfile = new Callback() {
+            @Override
+            public void onSuccess(Map<String, Object> data, String message) {
+                dbPublicFiles f = new dbPublicFiles((String) data.get("encryption_key"), (String) data.get("file_name"), (String) data.get("file_path"), (String) data.get("file_path"), (String) data.get("file_storage"), (String) data.get("user_id"));
+                f.setID(message);
+                publicFiles.add(f);
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(String error, MyError.ErrorCode errorCode) {
+                Log.e(TAG, errorCode.toString());
+            }
+        };
+
+        CollectionReference collection = db.collection("Public Files");
+
+        //Query for public files that belong to this friend
+        Query query = collection.whereEqualTo("user_id", friend.getDocID());
+        query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.e(TAG, "Listen failed.", e);
+                    return;
+                }
+
+                txtNumPublic.setText(Integer.toString(value.size()));
+                //Manually perform a join with the Users collection
+                for (QueryDocumentSnapshot doc : value) {
+                    cbPfile.onSuccess(doc.getData(), doc.getId());
+                }
+            }
+        });
+    }
+
+    //---------------------------------------------------------------------------------
+    //--------Classes and Interfaces for RecyclerView defined Beyond this point--------
+    //---------------------------------------------------------------------------------
+
+    public class RecyclerAdapter extends RecyclerView.Adapter<FriendProfileActivity.RecyclerAdapter.MyViewHolder> {
+
+        private Context context;
+        private ArrayList<dbPublicFiles> fileList;
+        private FriendProfileActivity.AdapterListener listener;
+
+        public RecyclerAdapter(Context context, ArrayList<dbPublicFiles> files, FriendProfileActivity.AdapterListener listener) {
+
+            fileList = files;
+            this.listener = listener;
+            this.context = context;
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder{
+            public TextView txt_filename;
+
+            public MyViewHolder(View view) {
+                super(view);
+                txt_filename = view.findViewById(R.id.txtbtn_filename);
+
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // send selected contact in callback
+                        listener.onFileSelected(fileList.get(getAdapterPosition()));
+                    }
+                });
+            }
+        }
+
+        @Override
+        public FriendProfileActivity.RecyclerAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.view_recycler_public_view_friend_files, parent, false);
+
+            return new FriendProfileActivity.RecyclerAdapter.MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(FriendProfileActivity.RecyclerAdapter.MyViewHolder holder, final int position) {
+            final dbPublicFiles file = fileList.get(position);
+            holder.txt_filename.setText(file.getFileName());
+        }
+
+        @Override
+        public int getItemCount() {
+            return fileList.size();
+        }
+    }
+
+    public interface AdapterListener {
+        void onFileSelected(dbPublicFiles file);
+    }
 }
--- app/src/main/java/com/sd/a3kleingroup/classes/db/dbPublicFiles.java
+++ app/src/main/java/com/sd/a3kleingroup/classes/db/dbPublicFiles.java
@@ -75,4 +75,8 @@ public class dbPublicFiles extends dbFile{
     public void setID(String id) {
         ID=id;
     }
+
+    public String getID() {
+        return ID;
+    }
 }
